018264a981de957bd227b80324e8b1685610eaf9
Reyadul Islam <reyadul.islam792@gmail.com>
2020-10-16 00:58:37 +0600

    curve

--- src/components/Home/Header/Header.js
+++ src/components/Home/Header/Header.js
@@ -5,7 +5,7 @@ import './Header.css';
 
 const Header = () => {
     return (
-        <div className="header-container" style={{backgroundColor:'#FBD062'}}>
+        <div className="header-container" style={{backgroundColor:'#FBD062',borderRadius:' 0 85%  85%/30% 0'}}>
             <Navbar></Navbar>
             <HeaderMain></HeaderMain>
         </div>
